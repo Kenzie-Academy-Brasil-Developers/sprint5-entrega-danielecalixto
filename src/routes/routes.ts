d23d58d4108b87ad51d55de4b322cf9fbb810291
Daniele Calixto <danielecalixtob@gmail.com>
2022-05-16 09:33:23 -0300

    correcting routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,8 +4,14 @@ const routes = Router();
 
 import createUserController from "../controllers/users/createUser.controller";
 import listUsersController from "../controllers/users/listUsers.controller";
+import showUserController from "../controllers/users/showUser.controller";
+import updateUserController from "../controllers/users/updateUSer.controller";
+import deleteUserController from "../controllers/users/deleteUser.controller";
 
 routes.post("/users", createUserController);
 routes.get("/users", listUsersController);
+routes.get("/users/:id", showUserController);
+routes.patch("/users/:id", updateUserController);
+routes.delete("/users/:id", deleteUserController);
 
 export default routes;
